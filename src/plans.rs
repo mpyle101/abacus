c7bfa2ecd7b26f2aa31e72c94b19ce4e0aa173b1
Michael Pyle <mpyle101@gmail.com>
2024-06-10 21:49:23 -0600

    Move convert into plans.rs

--- src/plans.rs
+++ src/plans.rs
@@ -1,7 +1,9 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
 use datafusion::arrow::datatypes::{DataType, TimeUnit};
+use datafusion::logical_expr::Operator;
 use datafusion::parquet::basic::Compression;
+use datafusion::prelude::*;
 use serde::Deserialize;
 
 #[derive(Clone, Copy, Debug, Deserialize)]
@@ -351,4 +353,45 @@ pub enum Expression<'a> {
 
     cast(Box<Expression<'a>>, SchemaDataType),
     sort(Box<Expression<'a>>, bool, bool),
+}
+
+pub fn convert(expr: &Expression) -> Expr
+{
+    match expr {
+        Expression::f32(v)  => lit(*v),
+        Expression::f64(v)  => lit(*v),
+        Expression::i32(v)  => lit(*v),
+        Expression::i64(v)  => lit(*v),
+        Expression::str(v) => lit(*v),
+        Expression::col(v) => col(format!(r#""{v}""#)),
+        Expression::abs(expr)  => abs(convert(expr)),
+        Expression::acos(expr) => acos(convert(expr)),
+        Expression::asin(expr) => atan(convert(expr)),
+        Expression::atan(expr) => atan(convert(expr)),
+        Expression::not(expr)  => not(convert(expr)),
+        Expression::and(exprs) => exprs.iter().map(convert).reduce(and).unwrap(),
+        Expression::or(exprs)  => exprs.iter().map(convert).reduce(or).unwrap(),
+        Expression::avg(exprs) => avg(make_array(exprs.iter().map(convert).collect())),
+        Expression::min(exprs) => min(make_array(exprs.iter().map(convert).collect())),
+        Expression::max(exprs) => max(make_array(exprs.iter().map(convert).collect())),
+        Expression::sum(exprs) => sum(make_array(exprs.iter().map(convert).collect())),
+        Expression::eq(exprs)  => binary_expr(convert(&exprs[0]), Operator::Eq, convert(&exprs[1])),
+        Expression::ne(exprs)  => binary_expr(convert(&exprs[0]), Operator::NotEq, convert(&exprs[1])),
+        Expression::gt(exprs)  => binary_expr(convert(&exprs[0]), Operator::Gt, convert(&exprs[1])),
+        Expression::gte(exprs) => binary_expr(convert(&exprs[0]), Operator::GtEq, convert(&exprs[1])),
+        Expression::lt(exprs)  => binary_expr(convert(&exprs[0]), Operator::Lt, convert(&exprs[1])),
+        Expression::lte(exprs) => binary_expr(convert(&exprs[0]), Operator::LtEq, convert(&exprs[1])),
+        Expression::add(exprs) => binary_expr(convert(&exprs[0]), Operator::Plus, convert(&exprs[1])),
+        Expression::sub(exprs) => binary_expr(convert(&exprs[0]), Operator::Minus, convert(&exprs[1])),
+        Expression::mul(exprs) => binary_expr(convert(&exprs[0]), Operator::Multiply, convert(&exprs[1])),
+        Expression::div(exprs) => binary_expr(convert(&exprs[0]), Operator::Divide, convert(&exprs[1])),
+        
+        Expression::is_true(expr)  => is_true(convert(expr)),
+        Expression::is_false(expr) => is_false(convert(expr)),
+        Expression::stddev(exprs)  => stddev(make_array(exprs.iter().map(convert).collect())),
+        Expression::modulus(exprs) => binary_expr(convert(&exprs[0]), Operator::Modulo, convert(&exprs[1])),
+        Expression::product(exprs) => exprs.iter().map(convert).reduce(|a, b| a * b).unwrap(),
+        Expression::cast(expr, dtype) => cast(convert(expr), (*dtype).into()),
+        Expression::sort(expr, asc, nulls_first) => convert(expr).sort(*asc, *nulls_first),
+    }
 }
